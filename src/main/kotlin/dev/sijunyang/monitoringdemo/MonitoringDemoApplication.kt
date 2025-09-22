4af504fd85d3fd2897c3578f524b0e23690f6214
sijun-yang <yangsijun5528@gmail.com>
2025-09-22 14:08:06 +0900

    init

---
+++ src/main/kotlin/dev/sijunyang/monitoringdemo/MonitoringDemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.sijunyang.monitoringdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitoringDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MonitoringDemoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/sijunyang/monitoringdemo/MonitoringDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.sijunyang.monitoringdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitoringDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
